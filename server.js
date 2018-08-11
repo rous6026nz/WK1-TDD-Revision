5d276a13c22470465f4169b4854e3def89e87369
Zaine Kingi <rous6026nz@gmail.com>
2018-08-11 18:01:50 +1200

    Add register, login, profile query string, profiles params routes, static file configuration and register and login test scripts.

--- server.js
+++ server.js
@@ -1,5 +1,46 @@
 const express = require('express')
 const server = express()
 
+// 
+// Static file directory configuration.
+// 
+server.use('dir', express.static('public'))
+
+// 
+// Root directory route.
+// 
+server.get('/', (req, res) => {
+    res.send('Hello out there!')
+})
+
+// 
+// /register route.
+// 
+server.get('/register', (req, res) => {
+    res.send('Register')
+})
+
+// 
+// /login route.
+// 
+server.get('/login', (req, res) => {
+    res.send('Login')
+})
+
+// 
+// /profile with name query string route.
+// 
+server.get('/profile', (req, res) => {
+    const name = req.query.name
+    res.send(name)
+})
+
+// 
+// /profiles parameter route.
+// 
+server.get('/profiles/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    res.send('Silvia')
+})
 
 module.exports = server
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,43 +1,90 @@
 const request = require('supertest')
 const server = require('../server')
 
+// 
 // Test root endpoint.
+// 
 test('/ test to see if we get a 200 status code', () => {
+
+	// Act
 	request(server)
   		.get('/')
   		.end((err, res) => {
+			
+			// Assert
     		expect(err).toBeNull()
-      		expect(res.status).toBe(200)
+      		expect(res.text).toBe('Hello out there!')
     	})
 })
 
+// 
+// Test login page endpoint.
+// 
+test('/login return a status code 200 and HTML mark-up', () => {
+
+	// Act
+	request(server)
+		.get('/login')
+		.end((err, res) => {
+
+			// Assert
+			expect(err).toBeNull()
+			expect(res.status).toBe(200)
+			expect(res.text).toBe('Login')
+		})
+})
+
+// 
+// Test registration page endpoint.
+// 
+test('/registration return a status code 200 and HTML mark-up', () => {
+
+	// Act
+	request(server)
+		.get('/register')
+		.end((err, res) => {
+
+			// Assert
+			expect(err).toBeNull()
+			expect(res.status).toBe(200)
+			expect(res.text).toBe('Register')
+		})
+})
+
+// 
 // Test profile with query endpoint.
-test('/profile?name=silvia test to check we get a 200 status code', () => {
+// 
+test('/profile?name=silvia test to check we get a 200 status code and silvia as a string', () => {
+
   	// Arrange
   	const query = '?name=silvia'
     
-    // Assert
+    // Act
     request(server)
   		.get('/profile' + query)
   		.end((err, res) => {
-    		expect(err).toBeNull()			// Make sure no errors occur.
-      		expect(res.status).toBe(200)	// Make sure a page is returned.
-      		expect(res.text).not.toBeNull()	// Make sure some HTML mark-up is returned.
+
+			// Assert
+    		expect(err).toBeNull()
+      		expect(res.status).toBe(200)
+			expect(res.text).toBe('silvia')
     	})
 })
 
+// 
 // Test profiles with id parameter.
+// 
 test('/profiles/:id returns a profile page', () => {
+
 	//Arrange
-  	const id = parseInt(req.params.id)
+  	const id = 2
     
   	// Act
-  	// Call supertest
     request(server)
   		.get('/profiles/' + id)
-  
-  	// Assert
   		.end((err, res) => {
+
+			// Assert
     		expect(err).toBeNull()
       		expect(res.status).toBe(200)
       		expect(res.text).toBe('Silvia')
